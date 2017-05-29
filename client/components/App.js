a03a50537b014d488ae8e198286d6866087be7a3
camilo17 <cami172013@gmail.com>
2017-05-28 22:10:15 -0500

    Added AuthForm to LoginForm.

--- client/components/App.js
+++ client/components/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 class App extends Component {
     render(){
         return (
-            <div>
+            <div className="container">
                 <Header/>
                 {this.props.children}
             </div>
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import AuthForm from './AuthForm'; 
 
 
 class LoginForm extends Component {
     render() {
         return (
             <div>
-                LoginForm 
+                <h3>Login</h3>
+                <AuthForm/>
             </div>    
         )
     }
